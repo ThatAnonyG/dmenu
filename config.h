39145198baa7dcc01357951820fb14edb9bfeca7
Ratul Saha <contact@ratulsaha.me>
2023-12-26 13:19:42 +0530

    patch: search match highlight

--- config.h
+++ config.h
@@ -11,10 +11,14 @@ static const char *prompt =
     NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
     /*     fg         bg       */
-    [SchemeNorm] = {"#cdd6f4", "#1e1e2e"},
-    [SchemeSel] = {"#1e1e2e", "#87B0F9"},
-    [SchemeOut] = {"#000000", "#87B0F9"},
-    [SchemeMid] = {"#cdd6f4", "#313244"},
+    [SchemeNorm] = {"#C6D0F5", "#1E1E2E"},
+    [SchemeNormHighlight] = {"#74C7EC", "#1E1E2E"},
+    [SchemeSel] = {"#1E1E2E", "#87B0F9"},
+    [SchemeSelHighlight] = {"#C6D0F5", "#87B0F9"},
+    [SchemeOut] = {"#1E1E2E", "#F9E2AF"},
+    [SchemeOutHighlight] = {"#F38BA8", "#F9E2AF"},
+    [SchemeMid] = {"#C6D0F5", "#313244"},
+    [SchemeMidHighlight] = {"#74C7EC", "#313244"},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines = 0;
